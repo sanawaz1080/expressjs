da149c630aa40d81521325c38358c94cd5fd9eee
Shaik Ahamd Nawaz <nawazcode@gmail.com>
2022-09-11 13:49:40 +0530

    basic navigation

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
+// Importing express
 const express = require("express");
+const path = require("path");
 const app = express();
-
-app.listen(3000, () => {
-  console.log("listening on 3000");
+// check if we kept any port in environment variable if not then listen to 3000 port
+const PORT = process.env.PORT || 3000;
+// We are getting the exact path by doing like this
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+app.get("/about", (req, res) => {
+  res.sendFile(__dirname + "/about.html");
+});
+app.listen(PORT, () => {
+  console.log(`listening on port  ${PORT}`);
 });
