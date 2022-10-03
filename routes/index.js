58cc7669a7e6866829983b073870c8d85d073a9a
Shaik Ahamd Nawaz <nawazcode@gmail.com>
2022-10-03 18:29:12 +0530

    Middlewares

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,5 @@
+const apiKeyMiddleware = require("../middlewares/apiKey");
+
 const router = require("express").Router();
 router.get("/", (req, res) => {
   res.render("index", {
@@ -13,6 +15,19 @@ router.get("/about", (req, res) => {
 // We can add download feature like this in express
 router.get("/download", (req, res) => {
   res.download(path.resolve(__dirname) + "/about.html");
+});
+
+router.get("/api/products", apiKeyMiddleware, (req, res) => {
+  res.json([
+    {
+      id: "123",
+      name: "chrome",
+    },
+    {
+      id: "124",
+      name: "brave",
+    },
+  ]);
 });
 
 module.exports = router;
