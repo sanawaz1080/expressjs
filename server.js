1c3dfeab0103ad6e10cf01eb059e397989d7b29b
Shaik Ahamd Nawaz <nawazcode@gmail.com>
2022-10-03 11:12:20 +0530

    download option

--- server.js
+++ server.js
@@ -9,12 +9,17 @@ const PORT = process.env.PORT || 3000;
 To serve static files such as images, CSS files, and JavaScript files, use the express.static built-in middleware function in Express. The function signature is: express. static(root, [options]) The root argument specifies the root directory from which to serve static assets. */
 app.use(express.static("public"));
 // We are getting the exact path by doing like this
-// app.get("/", (req, res) => {
-//   res.sendFile(__dirname + "/index.html");
-// });
-// app.get("/about", (req, res) => {
-//   res.sendFile(__dirname + "/about.html");
-// });
+// The path.resolve() method is used to resolve a sequence of path-segments to an absolute path.
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname) + "/index.html");
+});
+app.get("/about", (req, res) => {
+  res.sendFile(path.resolve(__dirname) + "/about.html");
+});
+// We can add download feature like this in express
+app.get("/download", (req, res) => {
+  res.download(path.resolve(__dirname) + "/about.html");
+});
 app.listen(PORT, () => {
   console.log(`listening on port  ${PORT}`);
 });
